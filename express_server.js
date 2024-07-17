21ccd3ee4455ecc044edc5416f81555015677ce1
jon-jh <hiebert456@gmail.com>
2024-07-17 16:20:23 -0600

    add urls:id page. using :id makes id the route parameter. it is set to point to the key-value pairs in urlDatabase.

--- express_server.js
+++ express_server.js
@@ -4,36 +4,42 @@ const port = 8080;
 
 app.set('view engine', 'ejs');
 
-//Init Server
+//init server
 app.listen(port, () => {
   console.log(`The example app is listning on port ${port}`);
 });
 
-//Home Page---localhost:8080
+//home page - localhost:8080
 app.get('/', (req, res) => {
   res.send('Hello there!');
 });
 
-//------------/urls.json
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
 
-app.get('/urls.json', (req, res) => {
-  res.json(urlDatabase);
-}); // This adds the web page but does not render it with EJS.
-
+//pages rendered with EJS
+//-/urls
 app.get('/urls', (req, res) => {
   const templateVars = {urls: urlDatabase};
   res.render('urls_index', templateVars);
-}); // New page, rendered with EJS.
+});
+//-/urls/:id
+app.get('/urls/:id', (req, res) => {
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]};
+  res.render('urls_show', templateVars);
+});
 
-//------------/hello
+//pages not EJS rendered
+//-/urls.json
+app.get('/urls.json', (req, res) => {
+  res.json(urlDatabase);
+});
+//-/hello
 app.get('/hello', (req, res) => {
   res.send('<html><body>Hello <b>World</b>, this is an HTML test. <body><html>\n');
 });
-
 
 
 
