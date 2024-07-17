92a26be9b6e7355226bf1fc77c4acb9f6dedcd5b
jon-jh <hiebert456@gmail.com>
2024-07-17 15:08:43 -0600

    insert ejs templating engine with views folder

--- express_server.js
+++ express_server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
+app.set('view engine', 'ejs');
+
 //Init Server
 app.listen(port, () => {
   console.log(`The example app is listning on port ${port}`);
