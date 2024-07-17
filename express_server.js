ea835da9ccb138acd781d16053a3bfdf8fe9216d
jon-jh <hiebert456@gmail.com>
2024-07-17 14:16:17 -0600

    organize code and add page with HTML use

--- express_server.js
+++ express_server.js
@@ -2,15 +2,31 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
+//Init Server
+app.listen(port, () => {
+  console.log(`The example app is listning on port ${port}`);
+});
+
+//Home Page---localhost:8080
+app.get('/', (req, res) => {
+  res.send('Hello there!');
+});
+
+//------------/urls.json
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
 
-app.get('/', (req, res) => {
-  res.send('Hello there!');
+app.get('/urls.json', (req, res) => {
+  res.json(urlDatabase);
 });
 
-app.listen(port, () => {
-  console.log(`The example app is listning on port ${port}`);
-});
+//------------/hello
+app.get('/hello', (req, res) => {
+  res.send('<html><body>Hello <b>World</b>, this is an HTML test. <body><html>\n')
+})
+
+
+
+
