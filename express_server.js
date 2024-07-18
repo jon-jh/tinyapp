806fd6c86b3aba2c0359ad057cf2fe8c68fcfb6e
jon-jh <hiebert456@gmail.com>
2024-07-18 13:00:38 -0600

    add new-url form submission page urls_new

--- express_server.js
+++ express_server.js
@@ -2,16 +2,16 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
+//Set up template engine (view folder) and urlencoded option for allowing POST data to be read(like form submission on the website.)
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended: true}));
+
+//generate 6 character string
+function generateRandomString() {}
 
 //init server
 app.listen(port, () => {
   console.log(`The example app is listning on port ${port}`);
-});
-
-//home page - localhost:8080
-app.get('/', (req, res) => {
-  res.send('Hello there!');
 });
 
 const urlDatabase = {
@@ -19,27 +19,58 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com",
 };
 
+
 //pages rendered with EJS
-//-/urls
+
+//---/urls
 app.get('/urls', (req, res) => {
   const templateVars = {urls: urlDatabase};
   res.render('urls_index', templateVars);
 });
-//-/urls/:id
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
+
+//We must add routes that are not route parameters (eg. urls/:id) before / above the route parameter.
+
+//---/urls/new
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
+});
+
+//This means when this form is submitted, it will make a request to POST / urls, and the body will contain one URL - encoded name - value pair with the name longURL.
+
+//---/urls/:id
 app.get('/urls/:id', (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]};
   res.render('urls_show', templateVars);
 });
 
+
 //pages not EJS rendered
-//-/urls.json
+
+//---home page---/localhost:8080
+app.get('/', (req, res) => {
+  res.send('Hello there!');
+});
+//---/urls.json
 app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
 });
-//-/hello
+//---/hello
 app.get('/hello', (req, res) => {
   res.send('<html><body>Hello <b>World</b>, this is an HTML test. <body><html>\n');
 });
 
 
+/*clarification notes
+
+- After our browser renders our new URL form, the user populates the form with a longURL and presses submit.
+- Our browser sends a POST request to our server.
+- Our server logs the request body to the console, then responds with 200 OK.
+- Our browser renders the "Ok" message.
+
+*/
 
