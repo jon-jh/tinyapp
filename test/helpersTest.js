2b32a9596755b039c42999862039fdb4005e61e8
jon-jh <hiebert456@gmail.com>
2024-07-26 21:03:17 -0600

    ai tests

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -26,3 +26,46 @@ describe('getUserByEmail', function() {
     assert.isUndefined(user);
   });
 });
+
+const urlsForUser = (user_id, urlDatabase) => {
+  const userFilter = {};
+  for (const shortURL in urlDatabase) {
+    if (urlDatabase[shortURL].nestedObjectID === user_id) {
+      userFilter[shortURL] = urlDatabase[shortURL];
+    }
+  }
+  return userFilter;
+};
+
+const testUrlDatabase = {
+  "b2xVn2": { nestedObjectID: "userRandomID", longURL: "http://www.lighthouselabs.ca" },
+  "9sm5xK": { nestedObjectID: "user2RandomID", longURL: "http://www.google.com" }
+};
+
+describe('urlsForUser', function() {
+  it('should return urls that belong to the specified user', function() {
+    const userUrls = urlsForUser("userRandomID", testUrlDatabase);
+    const expectedUrls = {
+      "b2xVn2": { nestedObjectID: "userRandomID", longURL: "http://www.lighthouselabs.ca" }
+    };
+    assert.deepEqual(userUrls, expectedUrls);
+  });
+
+  it('should return an empty object if the user has no urls', function() {
+    const userUrls = urlsForUser("nonexistentUser", testUrlDatabase);
+    assert.deepEqual(userUrls, {});
+  });
+
+  it('should return an empty object if there are no urls in the urlDatabase', function() {
+    const userUrls = urlsForUser("userRandomID", {});
+    assert.deepEqual(userUrls, {});
+  });
+
+  it('should not return urls that don\'t belong to the specified user', function() {
+    const userUrls = urlsForUser("userRandomID", testUrlDatabase);
+    const unexpectedUrls = {
+      "9sm5xK": { nestedObjectID: "user2RandomID", longURL: "http://www.google.com" }
+    };
+    assert.notDeepEqual(userUrls, unexpectedUrls);
+  });
+});
